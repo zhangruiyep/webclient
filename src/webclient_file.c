1b8f023086aea41cf0f4afefd657dd0828e7d512
zhangruiyep <zhangruiyep@outlook.com>
2019-10-10 17:26:17 +0800

    add wget test function

--- src/webclient_file.c
+++ src/webclient_file.c
@@ -16,7 +16,16 @@
 
 #ifdef RT_USING_DFS
 #include <dfs_posix.h>
+#else
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <lfs_intf.h>
+#include <cli.h>
+#endif
 
+#if 1
 #define DBG_ENABLE
 #define DBG_SECTION_NAME               "web.file"
 #ifdef WEBCLIENT_DEBUG
@@ -289,5 +298,26 @@ int wget(int argc, char** argv)
 #include <finsh.h>
 MSH_CMD_EXPORT(wget, Get file by URI: wget <URI> <filename>.);
 #endif /* FINSH_USING_MSH */
+
+
+static void test_cmd_wget_test(char *buf, int len, int argc, char **argv)
+{
+    wget(argc, argv);
+}
+
+// STATIC_CLI_CMD_ATTRIBUTE makes this(these) command(s) static
+const static struct cli_command cmds_user[] STATIC_CLI_CMD_ATTRIBUTE = {
+    { "wget", "Get file by URI.", test_cmd_wget_test},                                       
+};
+
+int helper_wget_test_cli_init(void)
+{
+    // static command(s) do NOT need to call aos_cli_register_command(s) to register.
+    // However, calling aos_cli_register_command(s) here is OK but is of no effect as cmds_user are included in cmds list.
+    // XXX NOTE: Calling this *empty* function is necessary to make cmds_user in this file to be kept in the final link.
+    //return aos_cli_register_commands(cmds_user, sizeof(cmds_user) / sizeof(cmds_user[0]));
+    return 0;
+}
+
 
 #endif /* RT_USING_DFS */
